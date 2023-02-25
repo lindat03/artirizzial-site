973d9f90ab0ca5b070a7aeba52b824fe41054ce3
Linda Tang <lindatang@Lindas-MacBook-Pro-3.local>
2023-02-25 16:21:59 -0500

    Added two text input

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,37 +2,75 @@ import React, { useState } from "react";
 import './Chat.css';
 
 function Chat() {
-  const [inputValue, setInputValue] = useState("");
+  const [themInputValue, setThemInputValue] = useState("");
+  const [meInputValue, setMeInputValue] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  const handleThemInputChange = (event) => {
+    setThemInputValue(event.target.value);
+  };
+
+  const handleMeInputChange = (event) => {
+    setMeInputValue(event.target.value);
   };
 
   const handleSubmit = (event) => {
+    console.log('submit button pressed')
     event.preventDefault();
-    setMessages([...messages, inputValue]);
-    setInputValue("");
+    if(themInputValue && meInputValue){
+      console.log('both values found')
+      setMessages([...messages, [themInputValue, 0], [meInputValue, 1]]);
+    } else if(themInputValue){
+      console.log('them input found')
+      setMessages([...messages, [themInputValue, 0]]);
+    } else if(meInputValue){
+      console.log('me input found')
+      setMessages([...messages, [meInputValue, 1]])
+    }
+    setThemInputValue("");
+    setMeInputValue("");
   };
 
   return (
     <div className="chat">
       <div className="message-history">
-        {messages.map((message, index) => (
-          <div key={index} className="message">
-            {message}
-          </div>
-        ))}
+        {messages.map((message, index) => {
+          const userIdentifier = message[1];
+          const messageContent = message[0];
+          const messageStyle = userIdentifier === 0 ? 'them-message' : 'me-message';
+          return (
+            <div key={index} className={messageStyle}>
+              {messageContent}
+            </div>
+          );
+        })}
       </div>
       <form onSubmit={handleSubmit} className="input-form">
         <input
+          className="them-input"
+          type="text"
+          placeholder="Type your love interest's message here..."
+          value={themInputValue}
+          onChange={handleThemInputChange}
+        />
+        <input
+          className="me-input"
           type="text"
           placeholder="Type your message here..."
-          value={inputValue}
-          onChange={handleInputChange}
+          value={meInputValue}
+          onChange={handleMeInputChange}
         />
         <button type="submit">Send</button>
       </form>
+      {/* <form className="me-input-form">
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          value={meInputValue}
+          onChange={handleMeInputChange}
+        />
+        <button type="submit">Send</button>
+      </form> */}
     </div>
   );
 }
