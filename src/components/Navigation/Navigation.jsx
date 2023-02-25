158d264bf73b00fbc70a0ca92772d99297edcb3f
max <maxzhang1119@gmail.com>
2023-02-25 15:38:55 -0500

    changed navigation bar height and app

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Navigation.css';
 
-function Navigation() {
-  
-
-  
+function Navigation({bar_height}) {
+  console.log(bar_height)
   return (
     <div className="navigation-container">
       <div className="navigation-header">
         <img src="logo.png" alt="Logo" />
       </div>
-      <div className="navigation-indicator"></div>
+      <div className="navigation-indicator" style={{height: `${bar_height}%`}}></div>
     </div>
   );
 }
